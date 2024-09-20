e48f89beec9e0c1b6c4b2ce09141272384d75a6f
Milen Tecle <m_tecle@hotmail.com>
2024-09-20 12:01:54 +0000

    static files dirs

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -199,6 +199,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
