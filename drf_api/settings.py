5b560aefdf64ae613b46b6ac77d58e0bc0b242f1
Milen Tecle <m_tecle@hotmail.com>
2024-09-25 19:50:05 +0000

    add print message to for debugging

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import re
 import os
+print(os.environ.get('DEV'))
 import dj_database_url
 
 if os.path.exists('env.py'):
