48c26e47bdcd07231e2a96b09d23e0841b55416a
Milen Tecle <m_tecle@hotmail.com>
2024-09-26 19:08:35 +0000

    update views in drf api

--- drf_api/views.py
+++ drf_api/views.py
@@ -5,7 +5,15 @@ from .settings import (
     JWT_AUTH_SECURE,
 )
 
-# dj-rest-auth logut view fix
+
+@api_view()
+def root_route(request):
+    return Response({
+        "message": "Welcome to my drf API!"
+    })
+
+
+# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
@@ -13,7 +21,7 @@ def logout_route(request):
         key=JWT_AUTH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 jan 1970 00:00:00 GMT',
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
         secure=JWT_AUTH_SECURE,
@@ -22,15 +30,9 @@ def logout_route(request):
         key=JWT_AUTH_REFRESH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 jan 1970 00:00:00 GMT',
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
         secure=JWT_AUTH_SECURE,
     )
-
     return response
-@api_view(['GET'])
-def root_route(request):
-    return Response({
-        "message": "Welcome to my drf API!"
-    })
