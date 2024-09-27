48de475ed84f81aca6b0ed8dcb7cd031fb9e6df4
Milen Tecle <m_tecle@hotmail.com>
2024-09-27 07:49:19 +0000

    add SESSION_COOKIE_SAMESITE

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,7 +67,11 @@ REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
 }
 
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
 
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
